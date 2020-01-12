87d56c7792e8519ab14b0efd6df5a75a50ab7d59
jaymes15 <jamesoluwatomisinomtosho@gmail.com>
2020-01-11 22:50:03 -0800

    first commit

---
+++ firstc/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace firstc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to my console sum calculator, to quit enter 'exit' ");
+            do
+            {
+                Console.Write("Please enter a number:");
+                string firstInput = Console.ReadLine();
+                if( firstInput == "exit")
+                {
+                    Console.WriteLine("Thank you for using my console sum calculator");
+                    break;
+                }
+                else
+                {
+                    int x = Convert.ToInt32(firstInput);
+                    Console.Write("Please enter another number:");
+                    string secondInput = Console.ReadLine();
+                    int y = Convert.ToInt32(secondInput);
+                    int answer = x+y;
+                    Console.WriteLine(answer);
+                }
+            }
+            while (true);
+        
+        }
+    }
+}
